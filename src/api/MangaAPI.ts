89bfd9ebaa5cb7a31937cf28129615331c77eb9c
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-11-11 20:30:29 +0100

    initial api structure

--- src/api/MangaAPI.ts
+++ src/api/MangaAPI.ts
@@ -1,3 +1,21 @@
+// interface MiniManga {
+//     URL: string; // URL to the Manga
+//     thumbnail: string; // URl to the mangas thumbnail
+// }
+//
+// interface Manga {
+//     // Each manga should only be fetched once
+//     title: string; // eg. Berserk Chapter 1
+//     author: string;
+//     pages: Array<string>; // URLs to images (sorted)
+//     tags: Array<string>; // Many-to-many
+//     date_added: number;
+//
+//     collection: string; // eg. Berserk
+//     story: string; // eg. Berserk Prototype (lives in conjunction with chapter)
+//     chapter: number | null; //
+// }
+
 export default class MangaAPI {
     public static SERVER_URL = process.env.REACT_APP_SERVER_URL;
 }
