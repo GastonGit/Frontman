7952b2a13b204eec4ccc58f210baf3b049e9878f
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-11-17 18:38:24 +0100

    convert App to a functional component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import '../styles/App.css';
 
-class App extends React.Component<unknown, unknown> {
-    constructor(props: unknown) {
-        super(props);
-    }
-
-    render(): JSX.Element {
-        return <div className="app"></div>;
-    }
+function App(): JSX.Element {
+    return <div className="app"></div>;
 }
 
 export default App;
