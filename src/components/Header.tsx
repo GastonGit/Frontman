512396715b684ae5ff4d03f27add424c96a1b683
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-11-23 14:52:21 +0100

    replace Grid usage in Header with Appbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import '../styles/Header.css';
-import { Grid } from '@mui/material';
+import { AppBar, Box, Toolbar, Typography } from '@mui/material';
 
 export default class Header extends React.Component<unknown, unknown> {
     render(): JSX.Element {
         return (
-            <header className="header">
-                <div className="header__content">
-                    <Grid container>
-                        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                            <a href="/" className="header__link">
-                                Frontman
-                            </a>
-                        </Grid>
-                    </Grid>
-                </div>
-            </header>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static">
+                    <Toolbar variant="dense">
+                        <Typography variant="h6">Frontman</Typography>
+                    </Toolbar>
+                </AppBar>
+            </Box>
         );
     }
 }
