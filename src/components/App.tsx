4587d83b74218632cbc5b9b06b3000ae87114958
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-11-23 14:58:57 +0100

    move ServerLogin to Header

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import '../styles/App.css';
-import ServerLogin from './ServerLogin';
 
 function App(): JSX.Element {
     return (
         <div className="app">
-            <div className="app__content">
-                <ServerLogin />
-            </div>
+            <div className="app__content"></div>
         </div>
     );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/Header.css';
 import { AppBar, Box, Toolbar, Typography } from '@mui/material';
+import ServerLogin from './ServerLogin';
 
 export default class Header extends React.Component<unknown, unknown> {
     render(): JSX.Element {
@@ -9,6 +10,9 @@ export default class Header extends React.Component<unknown, unknown> {
                 <AppBar position="static">
                     <Toolbar variant="dense">
                         <Typography variant="h6">Frontman</Typography>
+                        <Box sx={{ ml: 2 }}>
+                            <ServerLogin />
+                        </Box>
                     </Toolbar>
                 </AppBar>
             </Box>
