e996d3ce2e9b2dfcf4ac723694f8b132c8a769bc
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-11-23 14:35:25 +0100

    add grid usage to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import '../styles/Header.css';
+import { Grid } from '@mui/material';
 
 export default class Header extends React.Component<unknown, unknown> {
     render(): JSX.Element {
         return (
             <header className="header">
                 <div className="header__content">
-                    <a href="/" className="header__link">
-                        Frontman
-                    </a>
+                    <Grid container>
+                        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                            <a href="/" className="header__link">
+                                Frontman
+                            </a>
+                        </Grid>
+                    </Grid>
                 </div>
             </header>
         );
