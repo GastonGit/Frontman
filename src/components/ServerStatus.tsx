b3fc4edcb1a78100851d8c743ec2794159fc80d6
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-11-28 16:28:26 +0100

    replace ServerStatus p element with Typography

--- src/components/ServerStatus.tsx
+++ src/components/ServerStatus.tsx
@@ -1,10 +1,11 @@
 import { useAppSelector } from '../app/hooks';
 import React from 'react';
+import { Typography } from '@mui/material';
 
 function ServerStatus(): JSX.Element {
     const fixedServer = useAppSelector((state) => state.server.fixedValue);
 
-    return <p>Current Server: {fixedServer}</p>;
+    return <Typography>Current Server: {fixedServer}</Typography>;
 }
 
 export default ServerStatus;
