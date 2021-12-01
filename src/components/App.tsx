8c95de2be4119238ceef8f25b4fe638681f4ab83
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-12-01 15:16:43 +0100

    set a conditional image render in App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import '../styles/App.css';
+import imageExample from '../assets/image1.jpg';
+import { useAppSelector } from '../app/hooks';
 
 function App(): JSX.Element {
+    const connected = useAppSelector((state) => state.server.connected);
+
     return (
         <div className="app">
-            <div className="app__content"></div>
+            <div className="app__content">
+                {connected && <img src={imageExample} />}
+            </div>
         </div>
     );
 }
