5b7e5daaf152776f6c54933f90f2fc26b3146ce2
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-12-04 15:16:56 +0100

    convert Header to functional component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,37 +4,35 @@ import { AppBar, Box, Link, Toolbar } from '@mui/material';
 import ServerLogin from './ServerLogin';
 import ServerStatus from './ServerStatus';
 
-export default class Header extends React.Component<unknown, unknown> {
-    render(): JSX.Element {
-        return (
-            <Box sx={{ flexGrow: 1 }}>
-                <AppBar position="static">
-                    <Toolbar
-                        variant="dense"
-                        className="header__bar"
-                        sx={{ py: { xs: 1, md: 0.5 } }}
+export default function Header(): JSX.Element {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar
+                    variant="dense"
+                    className="header__bar"
+                    sx={{ py: { xs: 1, md: 0.5 } }}
+                >
+                    <Link
+                        href="/"
+                        variant="h6"
+                        className="title__link"
+                        sx={{ mr: 2 }}
                     >
-                        <Link
-                            href="/"
-                            variant="h6"
-                            className="title__link"
-                            sx={{ mr: 2 }}
-                        >
-                            Frontman
-                        </Link>
-                        <Box
-                            sx={{
-                                flexGrow: 1,
-                                alignItems: 'center',
-                                display: { xs: 'block', md: 'flex' },
-                            }}
-                        >
-                            <ServerLogin />
-                            <ServerStatus />
-                        </Box>
-                    </Toolbar>
-                </AppBar>
-            </Box>
-        );
-    }
+                        Frontman
+                    </Link>
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            alignItems: 'center',
+                            display: { xs: 'block', md: 'flex' },
+                        }}
+                    >
+                        <ServerLogin />
+                        <ServerStatus />
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 }
